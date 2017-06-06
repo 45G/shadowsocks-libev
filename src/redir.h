88c8fcdf0243cd7b2a74bae67e2d660669702bd8
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2017-06-06 03:48:19 +0300

    added extra server state

--- src/redir.h
+++ src/redir.h
@@ -63,6 +63,10 @@ typedef struct server {
 
     struct sockaddr_storage destaddr;
     ev_timer delayed_connect_watcher;
+    
+    int try_tfo;
+    int got_irep;
+    int got_frep;
 } server_t;
 
 typedef struct remote_ctx {
